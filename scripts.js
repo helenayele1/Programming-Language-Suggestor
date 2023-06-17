e51c3d40de8abe62d6747d791e4bd05242afac34
Padma Patil <padma@email.com>
2023-06-17 14:21:55 -0700

    add background box to form

---
+++ scripts.js
@@ -0,0 +1,27 @@
+
+function addEventListener ();
+document.querySelector("form").onsubmit = function (event) {
+    hideResultsAndError ();
+    event.preventDefault();
+
+
+}
+
+document.getElementbyID ('person-identification').value;
+document.getElementbyID ('gamer').value;
+document.getElementbyID ('skydiver').value;
+document.getElementbyID ('sportgame').value;
+document.getElementbyID ('coffee-type').value;
+
+if (person-identification === yes )
+
+
+if (personIdentification && gamer === 'yes' && skydiving === 'never') {
+    programmingLanguage = 'Ruby';
+  }
+  if else (personIdentification && gamer === 'no' && skydiving === 'yes') {
+programmingLanguage = C#;  
+
+} if else (personIdentification && gamer === 'no' && skydiving === "never" && coffee-type === 'None' && sportgame === 'soccer') {
+    programmingLanguage = Javascript;
+}

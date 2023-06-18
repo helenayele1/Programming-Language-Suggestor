25b81d9a8a19cab042e824ee8dc443f2ce2d9a17
Padma Patil <padma@email.com>
2023-06-17 22:26:01 -0700

    update else if statements in js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault();
-    hideResultsAndError();
   
-    let personIdentification = document.getElementById('personIdentification').checked;
+    let beginnerLevel = document.getElementById('beginnerLevel').checked;
     let gamer = document.getElementById('gamer').value;
     let skyDiver = document.getElementById('skyDiver').value;
     let sportGame = document.getElementById('sportGame').value;
@@ -10,25 +9,26 @@ document.querySelector("form").addEventListener("submit", function (event) {
   
     let programmingLanguage;
   
-    if (personIdentification && gamer === 'yes' && skyDiver === 'never' && coffeeType === 'none' && sportGame === 'basketball') {
+    if (beginnerLevel && gamer === 'yes' && skyDiver === 'never' && coffeeType === 'none' && sportGame === 'basketball') {
       programmingLanguage = 'Ruby';
-    } else if (personIdentification && gamer === 'no' && skyDiver === 'yes' && coffeeType === '1-2' && sportGame === 'soccer') {
+    } else if (beginnerLevel && gamer === 'yes' && skyDiver === 'yes' && coffeeType === '1-2' && sportGame === 'soccer') {
       programmingLanguage = 'C#';
-    } else if (personIdentification && gamer === 'no' && skyDiver === 'never' && coffeeType === 'over 2' && sportGame === 'soccer') {
-      programmingLanguage = 'Javascript';
-    } else if (personIdentification && gamer === 'yes' && skyDiver === 'yes' && coffeeType === 'none' && sportGame === 'basketball') {
-      programmingLanguage = 'Ruby';
-    } else if (personIdentification === true && gamer === 'no' && skyDiver === 'never' && coffeeType === '1-2' && sportGame === 'soccer') {
+    } else if (beginnerLevel && gamer === 'yes' && skyDiver === 'never' && coffeeType === 'over 2' && sportGame === 'soccer') {
       programmingLanguage = 'Javascript';
     } else {
-      programmingLanguage = 'C#';
+      programmingLanguage = 'Unknown';
     }
   
-   function displayResult (programmingLanguage) {
-      // Get the result element
-  let resultElement = document.getElementById('result');
-  resultElement.innerHTML = '<h2>Results:</h2><p>' + programmingLanguage + '</p>';
-
-   }
+    displayResult(programmingLanguage);
   });
+  
+  function displayResult(programmingLanguage) {
+    let resultElement = document.getElementById('result');
+    resultElement.innerHTML = '<h2>Results:</h2><p>' + programmingLanguage + '</p>';
+    resultElement.style.display = 'block';
+    
+    let sentence = document.createElement('p');
+    sentence.textContent = "The coding language for you is " + programmingLanguage + ".";
+  
+  }
   

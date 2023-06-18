a40a598e351e2e80662d44c06dd58ea3916db469
Padma Patil <padma@email.com>
2023-06-17 18:49:51 -0700

    edit option values in HTML

--- scripts.js
+++ scripts.js
@@ -1,27 +1,34 @@
-
-function addEventListener ();
-document.querySelector("form").onsubmit = function (event) {
-    hideResultsAndError ();
+document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault();
+    hideResultsAndError();
+  
+    let personIdentification = document.getElementById('personIdentification').checked;
+    let gamer = document.getElementById('gamer').value;
+    let skyDiver = document.getElementById('skyDiver').value;
+    let sportGame = document.getElementById('sportGame').value;
+    let coffeeType = document.getElementById('coffeeType').value;
+  
+    let programmingLanguage;
+  
+    if (personIdentification && gamer === 'yes' && skyDiver === 'never' && coffeeType === 'none' && sportGame === 'basketball') {
+      programmingLanguage = 'Ruby';
+    } else if (personIdentification && gamer === 'no' && skyDiver === 'yes' && coffeeType === '1-2' && sportGame === 'soccer') {
+      programmingLanguage = 'C#';
+    } else if (personIdentification && gamer === 'no' && skyDiver === 'never' && coffeeType === 'over 2' && sportGame === 'soccer') {
+      programmingLanguage = 'Javascript';
+    } else if (personIdentification && gamer === 'yes' && skyDiver === 'yes' && coffeeType === 'none' && sportGame === 'basketball') {
+      programmingLanguage = 'Ruby';
+    } else if (personIdentification === true && gamer === 'no' && skyDiver === 'never' && coffeeType === '1-2' && sportGame === 'soccer') {
+      programmingLanguage = 'Javascript';
+    } else {
+      programmingLanguage = 'C#';
+    }
+  
+   function displayResult (programmingLanguage) {
+      // Get the result element
+  let resultElement = document.getElementById('result');
+  resultElement.innerHTML = '<h2>Results:</h2><p>' + programmingLanguage + '</p>';
 
-
-}
-
-document.getElementbyId ('person-identification').checked;
-document.getElementbyId ('gamer').value;
-document.getElementbyId ('skydiver').value;
-document.getElementbyId ('sportgame').value;
-document.getElementbyId ('coffee-type').value;
-
-if (person-identification === yes )
-
-
-if (personIdentification && gamer === 'yes' && skydiving === 'never') {
-    programmingLanguage = 'Ruby';
-  }
-  if else (personIdentification && gamer === 'no' && skydiving === 'yes') {
-programmingLanguage = C#;  
-
-} if else (personIdentification && gamer === 'no' && skydiving === "never" && coffee-type === 'None' && sportgame === 'soccer') {
-    programmingLanguage = Javascript;
-}
+   }
+  });
+  
